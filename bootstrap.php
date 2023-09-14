69969ecb980b82dbff32cead50b1ba52872cf822
August Miller <hello@gusmiller.com>
2023-09-14 14:41:44 -0700

    Update Craft, use modern configuration

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,6 @@
  * Shared bootstrap file
  */
 
-use craft\helpers\App;
-
 // Define path constants
 define('CRAFT_BASE_PATH', __DIR__);
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
@@ -16,10 +14,3 @@ require_once CRAFT_VENDOR_PATH . '/autoload.php';
 if (class_exists('Dotenv\Dotenv')) {
     Dotenv\Dotenv::createUnsafeImmutable(CRAFT_BASE_PATH)->safeLoad();
 }
-
-// Define additional PHP constants
-// (see https://craftcms.com/docs/3.x/config/#php-constants)
-define('CRAFT_ENVIRONMENT', App::env('ENVIRONMENT') ?: 'production');
-define('CRAFT_LICENSE_KEY', App::env('CRAFT_LICENSE_KEY'));
-define('CRAFT_STORAGE_PATH', App::env('CRAFT_STORAGE_PATH'));
-define('CRAFT_STREAM_LOG', true);
