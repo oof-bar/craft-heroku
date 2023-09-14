e35e14ebf72c8c848f1f9d1a76923b69de563d88
August Miller <hello@gusmiller.com>
2023-09-14 15:29:35 -0700

    Simplify general config

--- config/general.php
+++ config/general.php
@@ -2,39 +2,11 @@
 /**
  * General Configuration
  *
- * All of your system's general configuration settings go in here. You can see a
- * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ * All of your system's general configuration settings go in here. You can see a list of the available settings in {@see craft\config\GeneralConfig}, or in the documentation.
  *
- * @see \craft\config\GeneralConfig
+ * @link https://craftcms.com/docs/4.x/config/general.html
  */
 
-use craft\helpers\App;
+use craft\config\GeneralConfig;
 
-$isDev = App::env('ENVIRONMENT') === 'dev';
-$isProd = App::env('ENVIRONMENT') === 'production';
-
-return [
-    // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 1,
-
-    // Whether generated URLs should omit "index.php"
-    'omitScriptNameInUrls' => true,
-
-    // The URI segment that tells Craft to load the control panel
-    'cpTrigger' => App::env('CP_TRIGGER') ?: 'admin',
-
-    // The secure key Craft will use for hashing and encrypting data
-    'securityKey' => App::env('SECURITY_KEY'),
-
-    // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
-    'devMode' => $isDev,
-
-    // Whether administrative changes should be allowed
-    'allowAdminChanges' => $isDev,
-
-    // Whether crawlers should be allowed to index pages and following links
-    'disallowRobots' => !$isProd,
-
-    // Disable the Queue over HTTP, in favor of a dedicated process
-    'runQueueAutomatically' => false,
-];
+return GeneralConfig::create();
